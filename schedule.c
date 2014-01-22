bb2e25488aa4e2ab29bf3115a5c67df12ca74cf6
sccalabr <sccalabr@calpoly.edu>
2014-01-22 11:36:22 -0800

    Update schedule.c

--- schedule.c
+++ schedule.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <schedule.h>
 
 int main(int argc, char **argv) {
 
    int quantum;
    
-
+   printf("Hello World %d %d", MAX_ARGUMENTS, MAX_PROCESSES);
 
 
 
