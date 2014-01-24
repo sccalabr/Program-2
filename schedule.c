07e4520e952b042e3f79633fe6e315b7a42d2927
sccalabr <sccalabr@calpoly.edu>
2014-01-23 21:46:38 -0600

    Update schedule.c

--- schedule.c
+++ schedule.c
@@ -2,11 +2,66 @@
 #include <stdlib.h>
 #include "schedule.h"
 
-int main(int argc, char **argv) {
+void *cleanArgs(char *args[]) {
+   int i = 0;
 
-   int quantum;
+   for(i = 0; i < MAX_ARGUMENTS; i++) {
+      args[i] = 0;
+   }
+}
+
+int main(int argc, char *argv[]) {
+
+   int quantum = atoi(argv[1]);
    
-   printf("Hello World %d %d", MAX_ARGUMENTS, MAX_PROCESSES);
+   int pid = 0;
+   int counter = 2;
+   int filenameFlag = 1;
+   int argCounter = 0;
+   
+   char *filename = 0;
+   char *args[MAX_ARGUMENTS + 1];
+
+   cleanArgs(args);
+   
+   while(counter < argc) {
+      // if we hit a colon we are going to need to forx and exec
+      if(!strcmp(argv[counter], ":")) {
+         if((pid = fork())) {
+            filenameFlag = 1;
+            argCounter = 0;
+            counter++;
+            cleanArgs(args);
+         }
+         else {
+            execvp(filename, args);
+         }
+      }
+   
+      // save the file name for exec
+      if(filenameFlag) {
+         filename = argv[counter];
+         filenameFlag = 0;
+      }
+      //save the arguments for exec
+      else {
+         args[argCounter++] = argv[counter];
+      }
+ 
+      counter++;
+   }
+   
+   // need to get the last one
+      if((pid = fork())) {
+         filenameFlag = 1;
+         argCounter = 0;
+         counter++;
+         cleanArgs(args);
+      }
+      else {
+         execvp(filename, args);
+      }
+
 
 
 
@@ -14,5 +69,7 @@ int main(int argc, char **argv) {
 
 
 }
+
+
 
 
